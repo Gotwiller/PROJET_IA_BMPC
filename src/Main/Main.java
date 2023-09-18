887e58bd85366f462500f2de45f97ab8963ef778
CercleTour <clement.bergere.38@gmail.com>
2023-09-18 16:54:47 +0200

    Quelque test au TP

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,11 +1,45 @@
 package Main;
 
+import lejos.hardware.Brick;
+import lejos.hardware.BrickFinder;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+
 public class Main {
 
+	/*
+		Brick brick = BrickFinder.getDefault();
+		brick.getPower().getVoltage();
+		
+		EV3LargeRegulatedMotor motorR = new EV3LargeRegulatedMotor(brick.getPort("C"));
+		EV3LargeRegulatedMotor motorL = new EV3LargeRegulatedMotor(brick.getPort("D"));
+
+		motorR.setSpeed(360);
+		motorL.setSpeed(360);
+
+		motorR.forward();
+		motorL.forward();
+
+		try {
+			Thread.sleep(1000);
+		} catch(Exception e) {}
+
+		motorR.stop();
+		motorL.stop();
+
+		motorR.close();
+		motorL.close();
+
+		EV3LargeRegulatedMotor pilers = new EV3LargeRegulatedMotor(brick.getPort("A"));
+
+		// angle positif = ouvrir ; angle négatif = fermer
+		pilers.rotate(360*2);
+	 */
+
 	public static void main(String[] args) {
-		//test
+		
 		// TODO : All, good luck guys *u*
 
 	}
-
 }
+
+
