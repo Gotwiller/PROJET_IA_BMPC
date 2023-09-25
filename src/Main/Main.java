14601ca10dfca100d786e98298d50b561c3594bf
CercleTour <clement.bergere.38@gmail.com>
2023-09-25 16:55:44 +0200

    WheeledChassis test

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,14 +2,17 @@ package Main;
 
 import lejos.hardware.Brick;
 import lejos.hardware.BrickFinder;
-import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.motor.Motor;
+import lejos.robotics.chassis.Chassis;
+import lejos.robotics.chassis.Wheel;
+import lejos.robotics.chassis.WheeledChassis;
 
 public class Main {
 
 	/*
 		Brick brick = BrickFinder.getDefault();
 		brick.getPower().getVoltage();
-		
+
 		EV3LargeRegulatedMotor motorR = new EV3LargeRegulatedMotor(brick.getPort("C"));
 		EV3LargeRegulatedMotor motorL = new EV3LargeRegulatedMotor(brick.getPort("D"));
 
@@ -36,10 +39,28 @@ public class Main {
 	 */
 
 	public static void main(String[] args) {
-		
+		Brick brick = BrickFinder.getDefault();
+		//EV3ColorSensor colorSensor = new EV3ColorSensor(brick.getPort("1"));
 		// TODO : All, good luck guys *u*
-
+	
+		Wheel leftWheel = WheeledChassis.modelWheel(Motor.D, 56).offset(-61.5);
+		Wheel rightWheel = WheeledChassis.modelWheel(Motor.C, 56).offset(61.5);
+		Chassis chassis = new WheeledChassis(new Wheel[]{leftWheel, rightWheel}, WheeledChassis.TYPE_DIFFERENTIAL);
+		//chassis.setLinearAcceleration(45);
+		//chassis.setLinearSpeed(180);
+	
+		chassis.rotate(-360*5);
+		while(chassis.isMoving()) {}
+		
+		/*
+		chassis.travel(1500);
+		while(chassis.isMoving()) {}
+		chassis.rotate(180);
+		while(chassis.isMoving()) {}
+		chassis.travel(1500);
+		while(chassis.isMoving()) {}
+		chassis.rotate(180);
+		while(chassis.isMoving()) {}
+		*/
 	}
 }
-
-
