85e730ea745cd01bca94f0c6eaa43b1aea17abcc
CercleTour <clement.bergere.38@gmail.com>
2023-09-26 20:23:35 +0200

    Modify the SuperClass of the ColorSensor from NXT to EV3

--- src/Robot/Sensor/ColorSensor.java
+++ src/Robot/Sensor/ColorSensor.java
@@ -1,9 +1,9 @@
 package Robot.Sensor;
 
 import lejos.hardware.port.Port;
-import lejos.hardware.sensor.NXTColorSensor;
+import lejos.hardware.sensor.EV3ColorSensor;
 
-public class ColorSensor extends NXTColorSensor {
+public class ColorSensor extends EV3ColorSensor {
 
 	public ColorSensor(Port p) {
 		super(p);
