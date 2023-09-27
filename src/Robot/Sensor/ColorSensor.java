54a52baf3d8198c2672e9cac4ef14e0a27269f91
CercleTour <clement.bergere.38@gmail.com>
2023-09-27 21:33:54 +0200

    Add getDetectedColor() method

--- src/Robot/Sensor/ColorSensor.java
+++ src/Robot/Sensor/ColorSensor.java
@@ -15,7 +15,8 @@ public class ColorSensor extends EV3ColorSensor {
 	private static final Map<String, int[]> COLORS = new HashMap<>();
 
 	private static final String calibratedColorsFileName = "colors.txt";
-	
+	private static final int RGB_TOLERANCE = 45;
+
 	/**
 	 * Instantiates the RGB color values with those saved in the colors.txt file
 	 */
@@ -23,7 +24,7 @@ public class ColorSensor extends EV3ColorSensor {
 		File file = new File(calibratedColorsFileName);
 		if(!file.exists())
 			throw new RuntimeException("Cannot initialize Map<String, int[]> COLORS from ColorSensor because colors.txt file is not found.");
-		
+
 		String[][] rgv_value = new String[7][];
 		try{
 			BufferedReader br = new BufferedReader(new FileReader(file));
@@ -57,7 +58,6 @@ public class ColorSensor extends EV3ColorSensor {
 				Integer.parseInt(rgv_value[4][1]),
 				Integer.parseInt(rgv_value[4][2])
 		});
-
 		COLORS.put("GRAY", new int[] {
 				Integer.parseInt(rgv_value[5][0]),
 				Integer.parseInt(rgv_value[5][1]),
@@ -72,5 +72,30 @@ public class ColorSensor extends EV3ColorSensor {
 
 	public ColorSensor(Port p) {
 		super(p);
+	}
+
+	/**
+	 * Get the name of the color detected.<p>
+	 * 
+	 * @return RED, GREEN, BLUE, YELLOW, BLACk, GRAY, WHITE, UNKNOWN
+	 */
+	public String getDetectedColor() {
+		float[] rgb = new float[3];
+		this.getRGBMode().fetchSample(rgb, 0);
+
+		rgb[0] = (int)(rgb[0]*255);
+		rgb[1] = (int)(rgb[1]*255);
+		rgb[2] = (int)(rgb[2]*255);
+
+		int[] calibatedColor;
+
+		for(int i = 0; i < COLOR_NAMES.length; i++) {
+			calibatedColor = COLORS.get(COLOR_NAMES[i]);
+			if	(	calibatedColor[0]-RGB_TOLERANCE < rgb[0] && rgb[0] < calibatedColor[0]+RGB_TOLERANCE &&
+					calibatedColor[1]-RGB_TOLERANCE < rgb[1] && rgb[1] < calibatedColor[1]+RGB_TOLERANCE &&
+					calibatedColor[2]-RGB_TOLERANCE < rgb[2] && rgb[2] < calibatedColor[2]+RGB_TOLERANCE 	)
+				return COLOR_NAMES[i];
+		}
+		return "UNKNOWN";
 	}
 }
