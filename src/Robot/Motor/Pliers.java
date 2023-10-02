472c993f911f3ee7dc4b2048d32abc5bc2ca7544
CercleTour <clement.bergere.38@gmail.com>
2023-10-02 19:08:00 +0200

    Pliers code optimization

--- src/Robot/Motor/Pliers.java
+++ src/Robot/Motor/Pliers.java
@@ -4,48 +4,64 @@ import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.Port;
 
 public class Pliers extends EV3LargeRegulatedMotor {
-	boolean closed = true;
+	
+	boolean closed;
+	
 	public Pliers(Port p) {
 		super(p);
+		closed = true;
 	}
 
+	/**
+	 * Open the pliers.
+	 */
 	public void open() {
 		open(false);
-	}	
+	}
+	/**
+	 * Open the pliers. You can force them to open.
+	 * 
+	 * @param force Forced opening
+	 */
 	public void open(boolean force) {
-		if (force==true) {
-			this.rotate(360*2);
-			closed = false;
-		}
-		if (isOpen()==false) {
+		if (closed || force) {
 			this.rotate(360*2);
 			closed = false;
 		}
 	}
+	
+	/**
+	 * Close the pliers.
+	 */
 	public void close() {
 		close(false);
 	}	
+	/**
+	 * Close the pliers. You can force them to close.
+	 * 
+	 * @param force Forced closing
+	 */
 	public void close(boolean force) {
-		if (force==true) {
-			this.rotate(-360*2);
-			closed = true;
-		}
-		if (isClose()==false) {
+		if (!closed || force) {
 			this.rotate(-360*2);
 			closed = true;
 		}
 	}
+	
+	/**
+	 * Check if the pliers is open.
+	 * 
+	 * @return true if open, else false
+	 */
 	public boolean isOpen() {
-		if (closed==true) {
-			return false;
-		}
-		else return true;
+		return !closed;
 	}
+	/**
+	 * Check if the pliers is closed.
+	 * 
+	 * @return true if close, else false
+	 */
 	public boolean isClose() {
-		if (closed==false) {
-			return false;
-		}
-		else return true;
+		return closed;
 	}
 }
-
