3d45079a2221c2b1e376310f9d615b76cbe603fa
admin <admin@DESKTOP-V724P9P>
2023-10-02 14:22:51 +0200

    classe pliers

--- src/Robot/Motor/Pliers.java
+++ src/Robot/Motor/Pliers.java
@@ -4,8 +4,13 @@ import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.Port;
 
 public class Pliers extends EV3LargeRegulatedMotor {
-
+	
 	public Pliers(Port p) {
 		super(p);
 	}
-}
+	public void open () {
+	pliers.rotate(360*2);
+}	
+	public void close() {
+	pliers.rotate(-360*2);
+	}
