65035b125ff795dbdd45f6664496edc67e7af571
admin <admin@DESKTOP-V724P9P>
2023-10-02 14:56:10 +0200

    pliers

--- src/Robot/Motor/Pliers.java
+++ src/Robot/Motor/Pliers.java
@@ -4,20 +4,32 @@ import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.Port;
 
 public class Pliers extends EV3LargeRegulatedMotor {
-	
+	boolean closed = true;
 	public Pliers(Port p) {
 		super(p);
 	}
 	public void open () {
-		this.rotate(360*2);
+		if (isOpen()==false) {
+			this.rotate(360*2);
+			closed = false;
+		}
 	}	
 	public void close() {
-		this.rotate(-360*2);
-	}
-	public boolean isClose() {
-		return 
+		if (isClose()==false) {
+			this.rotate(-360*2);
+			closed = true;
+		}
 	}
 	public boolean isOpen() {
-		return 
+		if (closed==true) {
+			return false;
+		}
+		else return true;
+	}
+	public boolean isClose() {
+		if (closed==false) {
+			return false;
+		}
+		else return true;
 	}
 }
