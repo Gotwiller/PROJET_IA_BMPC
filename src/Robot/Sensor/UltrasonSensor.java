a907125b1eb6d9009842d5d46bd8240f62d2ad82
alexandrechap <145359014+alexandrechap@users.noreply.github.com>
2023-10-16 16:11:59 +0200

    Update UltrasonSensor.java
    clarifySuspectDetection

--- src/Robot/Sensor/UltrasonSensor.java
+++ src/Robot/Sensor/UltrasonSensor.java
@@ -19,4 +19,26 @@ public class UltrasonSensor extends EV3UltrasonicSensor {
 	    getDistanceMode().fetchSample(front, 0);
 	    return (int)(front[0]*1000);
 	}
+
+	/**
+	 * clarify if the detection is a puck , a wall/robot/angle or if it's a wrong detection
+	 * 
+	 * @param expectedDistance
+	 * @return 	0 = not a puck, need to rotate (close to wall, robot or angle)
+	 *     		1 = nothing, continue
+	 *     		2 = puck
+	 */
+	
+	public int clarifySuspectDetection(int expectedDistance) {
+	int detected = getDetectedDistance(), erreur = 50;
+	try {Thread.sleep(10);}
+	catch (Exception e) {}
+	int detected2 = getDetectedDistance();
+	if (expectedDistance >= detected2-erreur && expectedDistance <= detected2+erreur)
+		return 0;
+	if (expectedDistance <= detected2-erreur && expectedDistance >= detected2+erreur && detected >= detected2-erreur && detected <= detected2+ erreur)
+		return 2;
+	else return 1;
+	}
+	
 }
